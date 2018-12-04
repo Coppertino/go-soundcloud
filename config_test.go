6ca6f3033a9552b043f2599c71eb05209f1a9feb
Andrey Semerun <andrey.semerun@gmail.com>
2018-12-04 18:52:11 +0200

    Updated package structure

--- config_test.go
+++ config_test.go
@@ -6,7 +6,7 @@ package soundcloud
 // And you'll have to authorize with that client to get a token
 // You don't NEED access tokens per se but some requests require it.
 
-var TestConfig map[string]interface{} = map[string]interface{}{
+var TestConfig = map[string]interface{}{
 	"client_id":     "f6212fc8e1e90501e7cece44f78b8f43",
 	"client_secret": "45d1d26f8399fc4c19abcf14d3830ff2",
 	"access_token":  "1-64086-73163832-dc46009aebdb123",
